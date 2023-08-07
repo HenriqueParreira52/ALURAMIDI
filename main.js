c98221d5d02be2a94756cad3ba271bf6baf03883
Henrique Parreira <134567951+HenriqueParreira52@users.noreply.github.com>
2023-08-07 15:47:17 -0300

    Add files via upload
    Projeto de uma controladora de som feito no curso de JavaScript para Web da Alura, em que consiste que obtenhamos mais conhecimentos com as funcionalidades da linguagem. O projeto consiste em uma controladora que emite os sons respectivos de cada tecla clicada.

---
+++ main.js
@@ -0,0 +1,29 @@
+function tocaSom (seletorElemento) {
+    const elemento = document.querySelector(seletorElemento);    
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
+};
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = function ( event ) {
+        if ( event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }    
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+} 
